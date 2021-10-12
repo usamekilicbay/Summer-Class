b926e9d23ec4bc4bf1afc7458d64e4ad7c4fb186
Usame Kilicbay <usame.kilicbay@gmail.com>
2021-10-12 12:55:44 +0300

    SessionHelper moved under Sidekick Project (a.k.a Helper)

---
+++ Helper/Session.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static Helper.Types;
+using static System.Web.HttpContext;
+
+namespace Helper
+{
+    public static class Session
+    {
+        public static SessionRole GetCurrentSessionRole()
+        {
+            if (Current.Session[SessionRole.TEACHER.ToString()] != null)
+                return SessionRole.TEACHER;
+            else if (Current.Session[SessionRole.STUDENT.ToString()] != null)
+                return SessionRole.STUDENT;
+            else
+                return SessionRole.NON;
+        }
+
+        public static string GetSessionKey(SessionRole sessionRole)
+        {
+            return Enum.GetName(typeof(SessionRole), sessionRole);
+        }
+    }
+}
